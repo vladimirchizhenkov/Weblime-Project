73cf34eaabdf1dc68d5854360af8ca886f225e48
vladimir_ch <w.chizhenkov@gmail.com>
2018-02-25 18:46:20 +0400

    add jq career select

--- js/nicescroll__settings.js
+++ js/nicescroll__settings.js
@@ -2,11 +2,11 @@ $("body").niceScroll({
 	cursorcolor: "#d8d8d8",
 	cursorwidth:"5px",
 	horizrailenabled: "false",
+	cursorborder:"none",
 	railvalign: "bottom"
 });
 
-$(function() {  
-    $("#nicescrollBox").niceScroll({
-    	cursorcolor:"#d8d8d8",
-    });
+$("#nicescrollBox").niceScroll({
+	cursorcolor:"#d8d8d8",
+	cursorborder:"none"
 });
