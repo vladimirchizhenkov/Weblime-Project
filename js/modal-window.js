7b75f8bc043402d57ff1302e756814c30fe16bb6
vladimir_ch <w.chizhenkov@gmail.com>
2018-02-27 01:38:58 +0400

    modal map window complete

--- js/modal-window.js
+++ js/modal-window.js
@@ -14,3 +14,20 @@
  		modal.style.display = "none";
  	}
  }
+
+ var modal_map = document.getElementById('on-map__window');
+ var btn_map = document.getElementById("on-map__link");
+ var span_map = document.getElementsByClassName("on-map__close")[0];
+
+ btn_map.onclick = function () {
+ 	modal_map.style.display = "block";
+ }
+ span_map.onclick = function () {
+ 	modal_map.style.display = "none";
+ }
+
+ window.onclick = function(event) {
+ 	if (event.target == modal_map) {
+ 		modal.style.display = "none";
+ 	}
+ }
