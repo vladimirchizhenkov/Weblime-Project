1574d64e945703ee00693665e4b2f7c06622911c
vladimir_ch <w.chizhenkov@gmail.com>
2018-03-09 01:35:39 +0400

    #3.1 [main-dev] start edit mobile menu. add hide jquery value

--- js/jquery__page-function.js
+++ js/jquery__page-function.js
@@ -4,4 +4,8 @@ $(".ladder-box").click(function(e) {
 	$(".ladder-block__date").removeClass("ladder-block__date_active")
 	$(this).addClass("ladder-box_active");
 	$(this).parent().find(".ladder-block__date").addClass("ladder-block__date_active");
-})
+});
+
+// $(".my-contacts-hidden").click(function() {
+// 	$(".contacts__list").css({opacity: 1, visibility:'visible'});
+// });
