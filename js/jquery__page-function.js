b775ed439345d5c99fdfae05a140ad66ab39cdf1
vladimir_ch <w.chizhenkov@gmail.com>
2018-03-13 03:06:58 +0400

    #4.1 [hot-fix] bug fix hover on "my contacts" in mobile"

--- js/jquery__page-function.js
+++ js/jquery__page-function.js
@@ -2,10 +2,10 @@ $(".ladder-box").click(function(e) {
 	e.preventDefault();
 	$(".ladder-box").removeClass("ladder-box_active");
 	$(".ladder-block__date").removeClass("ladder-block__date_active")
-	$(this).addClass("ladder-box_active");
+	$(this).addClass("ladder-box_active")
 	$(this).parent().find(".ladder-block__date").addClass("ladder-block__date_active");
 });
 
-// $(".my-contacts-hidden").click(function() {
-// 	$(".contacts__list").css({opacity: 1, visibility:'visible'});
-// });
+$(".my-contacts-hidden").click(function() {
+	$(".my-contacts-hidden__list").toggleClass("my-contacts-hidden__list_visible")
+});
