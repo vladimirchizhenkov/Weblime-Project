e68b597070f937cd861f6ad1410b38c6ea6a75f4
vladimir_ch <w.chizhenkov@gmail.com>
2018-03-15 01:55:04 +0400

    #4.2 [hot-fix] Reedit feedback mobile size. Fixed bug with feedback modal open/close

--- js/modal-window.js
+++ js/modal-window.js
@@ -2,6 +2,15 @@
  var btn_contacts = document.getElementById("feedback__button");
  var span_contacts = document.getElementsByClassName("feedback__close")[0];
 
+ var hidden_modal_contacts = document.getElementById('hidden-feedback__window');
+ var hidden_btn_contacts = document.getElementById("hidden-feedback__button");
+ var hidden_span_contacts = document.getElementsByClassName("hidden-feedback__close")[0];
+
+ var modal_map = document.getElementById('on-map__window');
+ var btn_map = document.getElementById("on-map__link");
+ var span_map = document.getElementsByClassName("on-map__close")[0];
+
+
  btn_contacts.onclick = function () {
  	modal_contacts.style.display = "block";
  }
@@ -15,9 +24,18 @@
  	}
  }
 
- var modal_map = document.getElementById('on-map__window');
- var btn_map = document.getElementById("on-map__link");
- var span_map = document.getElementsByClassName("on-map__close")[0];
+ hidden_btn_contacts.onclick = function () {
+ 	hidden_modal_contacts.style.display = "block";
+ }
+ hidden_span_contacts.onclick = function () {
+ 	hidden_modal_contacts.display = "none";
+ }
+
+ window.onclick = function(event) {
+ 	if (event.target == modal_contacts) {
+ 		hidden_modal_contacts.style.display = "none";
+ 	}
+ }
 
  btn_map.onclick = function () {
  	modal_map.style.display = "block";
